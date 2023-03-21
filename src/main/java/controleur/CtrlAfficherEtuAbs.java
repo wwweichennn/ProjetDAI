772e57da2d9ceb6b229a2dad18ca2bd3b5dd1ff3
Kevin_F <kevinfolvert@gmail.com>
2023-03-21 06:51:43 -0400

    Commit Debut Acceuil + Creation du controlleur afficheEtu

--- src/main/java/controleur/CtrlAfficherEtuAbs.java
+++ src/main/java/controleur/CtrlAfficherEtuAbs.java
@@ -1,5 +1,41 @@
 package controleur;
 
-public class CtrlAfficherEtuAbs {
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CtrlAfficherEtuAbs
+ */
+@WebServlet("/CtrlAfficherEtuAbs")
+public class CtrlAfficherEtuAbs extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		//Lecture des paramètres
+		String action = request.getParameter("type_promo");
+		
+		//Actions a effectuer
+		switch(action) {
+		case "IPM" :
+			//Chargement des etudiants concerné dans la Bd
+			
+			
+			//Pour un chainage vers page des IPM :
+			request.getRequestDispatcher("EtuAbsentIPM").forward(request, response);
+		}
+	}
+
+	/**
+	 * doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException { doGet(request, response); }
 
 }
---
+++ src/main/webapp/AcceuilScolarite.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Scolarite</title>
+</head>
+<body>
+
+	<nav></nav>
+	<h1>Scolarite</h1>
+	
+	<h2>Choisissez la formation à gérer : </h2>
+	<ul>
+		<li> <a href="CtrlAfficherEtuAbs?type_promo=IPM">IPM </a> </li>
+		<li> <a href="CtrlAfficherEtuAbs?type_promo=ISIAD">ISIAD </a></li>
+	</ul>
+
+</body>
+</html>
