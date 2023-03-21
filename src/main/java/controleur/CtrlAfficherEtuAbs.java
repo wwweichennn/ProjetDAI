69d38117f8a6c4512d2d731264483e242d3323e2
Kevin_F <kevinfolvert@gmail.com>
2023-03-21 16:12:44 -0400

    controleur afficher les abscense non valide

--- src/main/java/controleur/CtrlAfficherEtuAbs.java
+++ src/main/java/controleur/CtrlAfficherEtuAbs.java
@@ -1,12 +1,18 @@
 package controleur;
 
 import java.io.IOException;
+import java.util.*;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.hibernate.*;
+import org.hibernate.cfg.*;
+
+import metier.Etudiant;
 /**
  * Servlet implementation class CtrlAfficherEtuAbs
  */
@@ -18,20 +24,49 @@ public class CtrlAfficherEtuAbs extends HttpServlet {
 	 * doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+
 		//Lecture des paramètres
 		String action = request.getParameter("type_promo");
-		
-		//Actions a effectuer
-		switch(action) {
-		case "IPM" :
-			//Chargement des etudiants concerné dans la Bd
-			
-			
-			//Pour un chainage vers page des IPM :
-			request.getRequestDispatcher("EtuAbsentIPM").forward(request, response);
+
+		//		//Actions a effectuer
+		//		switch(action) {
+		//		case "IPM" :
+		//			//Chargement des etudiants concerné dans la Bd
+		//			
+		//			
+		//			//Pour un chainage vers page des IPM :
+		//			request.getRequestDispatcher("EtuAbsentIPM").forward(request, response);
+		//		}
+
+		//mappage des classes
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(metier.Etudiant.class)
+				.addAnnotatedClass(metier.Justificatif.class)
+				.buildSessionFactory();
+
+		Session session = factory.getCurrentSession();
+
+		try {
+			session.beginTransaction();
+
+			String queryString = "FROM Etudiant WHERE promotion=" + action + " AND nonValide=true";		//Requete pour recupérer les étudiants
+
+			List<Etudiant> etudiants = session.createQuery(queryString).getResultList();		//recupere le résultat de la requete
+
+			request.setAttribute("etudiants", etudiants);	//cree un attribut étudiant et affecte la valeur
+			request.getRequestDispatcher("VerifJustification.jsp").forward(request, response);		//chainage
+
+			session.getTransaction().commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			session.getTransaction().rollback();
+		} finally {
+			session.close();
+			factory.close();
 		}
-	}
+	}	
+
 
 	/**
 	 * doPost(HttpServletRequest request, HttpServletResponse response)
